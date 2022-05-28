2b39e3a6325e51fbeb28c1184bf099c8a5072e88
wangp79 <104248516+wangp79@users.noreply.github.com>
2022-05-28 13:04:16 +1000

    final_tested version
    tested version

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,15 +7,14 @@ from statistics import mean
 counter = 0
 net_total_amount = 0
 Average_Change = 0
-percentage_profit = 0 
 new_row_list = []
-final_number_list = []
-number = 0
-x = 0
-y = 0
+months = []
+
+# reading csv file
 budget_csv =os.path.join("Resources", "budget_data.csv")
 path =  "/Users/tony/Documents/GitHub/python-challenge/PyBank/Resources/budget_data.csv"
 
+# open csv file as read only
 with open (budget_csv,'r') as csv_file:
     print(csv_file)
     csv_reader = csv.reader(csv_file, delimiter=",")
@@ -24,45 +23,27 @@ with open (budget_csv,'r') as csv_file:
     print("Financial Analysis")
     print("------------------------")
     for row in csv_reader:
+        months.append(row[0]) # import all months name into a seperated new list
         counter = counter +1
-        net_total_amount += float(row[1])
-        Average_Change = net_total_amount/counter
-        new_row_list.append(int(row[1])) # import to a list named: new_row_list
+        net_total_amount += float(row[1]) # calculate total amount
+        new_row_list.append(int(row[1])) # import all months profit value into a new list and convert to Intiger
         
+print(f"Total Months:{counter}")
+print(f"Total: {net_total_amount}")
 
-#print(new_row_list)  
-#print(len(new_row_list))
-#print(new_row_list[4])
 differences = []
-max_1 = 0 
-index_of_max = 0 
-for i  in range(0,len(new_row_list)):
+for i  in range(0,len(new_row_list)): 
     if(i+1!=len(new_row_list)):
         current_value = new_row_list[i]
         next_value = new_row_list[i+1]
         
-        differences_value = current_value - next_value
-        if(max_1 < differences_value):
-            max_1 = differences_value
-            index_of_max  = i
+        differences_value = next_value - current_value # calculating profits between each month
+        differences.append(differences_value) # put all compared profit into a new list
         
-print(index_of_max)   
-print(max_1)
-        #print(current_value)
-#print(differences)
-#print(f"max number: {max(differences)}")
+max_index = differences.index(max(differences)) # return the highest profit increase month's index
+min_index = differences.index(min(differences)) # return the hightest profit decrease month's index
+Average_Change = float(mean(differences)) # calculate the mean value among each month's profit differences
 
-        
-
-    
-            
-
-#         #x = new_row_list[x+1] - new_row_list[x]
-#         #final_number_list.append(x)
-        
-
-
-        
-# print(f"Total Months:{counter}")
-# print(f"Total: {net_total_amount}")
-# print(f"Average Change: {Average_Change}")
+print(f"Average Change:{Average_Change}")
+print(f"Greatest Increase in Profits:{months[max_index+1]} ({max(differences)})")
+print(f"Greatest Decrease in Profits:{months[min_index+1]}({min(differences)}")
